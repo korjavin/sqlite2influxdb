b58bbc8c9e61a0ed573beaf91169db19e26e8b89
Korjavin Ivan <korjavin@gmail.com>
2015-06-10 14:21:45 +0500

    with timestamp

--- export.py
+++ export.py
@@ -1,11 +1,34 @@
 from influxdb.influxdb08 import InfluxDBClient
 db = InfluxDBClient('127.0.0.1', '8086', 'root', 'root', 'db1')
 
-data = [
-  {"points":[[1],[2]],
-   "name":"orders",
-   "columns":["a"]
-  }
-]
+import datetime
+import time
 
-db.write_points(data)
+import sqlite3 as lite
+con = lite.connect('test.db')
+#con = lite.connect('test.db',detect_types=lite.PARSE_DECLTYPES)
+with con:
+
+    cur = con.cursor()
+    cur.execute("SELECT os.regdt, os.sid, o.oid,o.amount,o.pid FROM Orders o inner join orderstatus os")
+
+
+    while True:
+
+        row = cur.fetchone()
+
+        if row == None:
+            break
+#2015-05-03 15:48:38
+#        mytime=time.mktime(datetime.datetime.strptime(row[0],'%Y-%m-%d %H:%M:%S').timetuple())
+        t = datetime.datetime.strptime(row[0],'%Y-%m-%d %H:%M:%S')
+        mytime=time.mktime(t.timetuple())
+
+        data = [
+                {"points":[[mytime,row[1],row[2],row[3],row[4]]],
+                    "name":"orders",
+                    "columns":["time","sid","oid","amount","pid"]
+                    }
+                ]
+
+        db.write_points(data)
